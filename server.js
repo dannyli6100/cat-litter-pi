25f8c72c5a52920d9813a877b6b30f82a46f0196
Danny Li <dannyli6100@gmail.com>
2024-08-13 20:37:55 -0700

    adjusted app structure to no longer use webrtc, as it is needlessly complex for this small app, now using rtmp server

--- server.js
+++ server.js
@@ -2,89 +2,76 @@ console.log(require("dotenv").config({ debug: true }));
 const express = require("express"); // web server
 const http = require("http"); // server instance
 const socketIo = require("socket.io"); // real-time communication
-const wrtc = require("wrtc"); // webrtc library
-const Stream = require("node-rtsp-stream");
+const NodeMediaServer = require("node-media-server");
+const { StreamCamera, Codec } = require("pi-camera-connect");
+const fs = require("fs");
 
 const app = express(); // create express app
 const server = http.createServer(app); // create http server
 const io = socketIo(server); // attach socket.io to server
 
-const peerConnections = new Map(); // map of peer connections, keyed with socket ids
-
 const piIP = process.env.RASPBERRY_PI_IP;
 const port = process.env.PORT;
 
-const stream = new Stream({
-  name: "stream",
-  streamUrl: "rtsp://",
+const config = {
+  rtmp: {
+    port: 1935, // standard rtmp port, The ffmpeg process streams video to this port,
+    // and RTMP clients can connect to this port to receive the stream.
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+  },
+  http: {
+    port: 8000, // standard http port
+    allow_origin: "*",
+  },
+};
+
+// create new instance of node-media-server
+var nms = new NodeMediaServer(config);
+// run trmp server
+nms.run();
+
+const streamCamera = new StreamCamera({
+  codec: Codec.H264,
 });
+
+const startStreaming = async () => {
+  const videoStream = streamCamera.createStream();
+
+  const streamToRTMP = fs.createWriteStream(`rtmp://${piIP}:1935/live/stream`);
+
+  videoStream.pipe(streamToRTMP);
+
+  await streamCamera.startCapture();
+
+  console.log("streaming started");
+};
+
+startStreaming().catch((err) => console.error(err));
 
 // listen for socket.io connections
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  let peerConnection; // declare peer connection variable so it's available to all event handlers in connection
-
   socket.on("join", async (roomId) => {
     // join event handler
     socket.join(roomId);
     console.log("user joined room " + roomId);
-
-    peerConnection = new wrtc.RTCPeerConnection({
-      // on user join, create peer connection
-      iceServers: [
-        {
-          urls: "stun:stun.l.google.com:19302",
-        },
-      ],
-    });
-
-    peerConnections.set(socket.id, peerConnection);
-
-    // video track from raspberry pi need to look it up
-
-    // ice candidate event handler
-    peerConnection.onicecandidate = (event) => {
-      if (event.candidate) {
-        socket.emit("ice-candidate", event.candidate);
-      }
-    };
-
-    try {
-      const offer = await peerConnection.createOffer();
-      await peerConnection.setLocalDescription(offer);
-    } catch (err) {
-      console.log(err);
-    }
-    socket.emit("offer", offer);
-  });
-
-  // answer event handler
-  socket.on("answer", async (answer) => {
-    try {
-      await peerConnection.setRemoteDescription(answer);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-
-  // ice candidate event handler
-  socket.on("ice-candidate", async (candidate) => {
-    try {
-      await peerConnection.addIceCandidate(candidate);
-    } catch (err) {
-      console.log(err);
-    }
+    // send stream ready url to client
+    socket.emit("stream-ready", `rtmp://${piIP}:1935/live/stream`);
   });
 
   // disconnect event handler
   socket.on("disconnect", () => {
     console.log("a user disconnected");
-    if (peerConnection) {
-      peerConnection.close();
-      peerConnections.delete(socket.id);
-    }
   });
+});
+
+process.on("SIGINT", async () => {
+  await streamCamera.stopCapture();
+  process.exit();
 });
 
 // start server and listen on port
